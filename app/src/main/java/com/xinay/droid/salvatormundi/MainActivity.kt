5259913e7122f015b1dd0ec6d23b926f7ce596e1
LuiX <luis.a.vivero@gmail.com>
2019-06-22 23:33:47 -0700

    Connect MainActivity to the MediaBrowserService

--- app/src/main/java/com/xinay/droid/salvatormundi/MainActivity.kt
+++ app/src/main/java/com/xinay/droid/salvatormundi/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.xinay.droid.salvatormundi
 
+import android.content.ComponentName
 import android.content.Context
 import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.media.AudioManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaControllerCompat
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.AnimationUtils
@@ -14,6 +18,7 @@ import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mediaBrowser: MediaBrowserCompat
     private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,16 +29,35 @@ class MainActivity : AppCompatActivity() {
         textView.setText("Salvator Mundi")
         textView.setTextColor(Color.YELLOW)
 
+        // Create MediaBrowserServiceCompat
+        mediaBrowser = MediaBrowserCompat(
+            this,
+            ComponentName(this, MediaPlaybackService::class.java),
+            connectionCallbacks,
+            null // optional Bundle
+        )
     }
 
     override fun onResume() {
         super.onResume()
+
+        volumeControlStream = AudioManager.STREAM_MUSIC
+
         AnimationUtils.loadAnimation(this, R.anim.jump).also { jump ->
             textView.startAnimation(jump)
         }
     }
 
 
+    public override fun onStart() {
+        super.onStart()
+        mediaBrowser.connect()
+    }
 
-
+    public override fun onStop() {
+        super.onStop()
+        // (see "stay in sync with the MediaSession")
+        MediaControllerCompat.getMediaController(this)?.unregisterCallback(controllerCallback)
+        mediaBrowser.disconnect()
+    }
 }
