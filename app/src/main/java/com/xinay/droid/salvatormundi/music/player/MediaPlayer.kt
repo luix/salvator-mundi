0d359983c2f789bb499a8bda9eabc460eda2783a
LuiX <luis.a.vivero@gmail.com>
2019-06-23 22:17:48 -0700

    Adding a MediaPlayer interface and it's implementation class

---
+++ app/src/main/java/com/xinay/droid/salvatormundi/music/player/MediaPlayer.kt
@@ -0,0 +1,15 @@
+package com.xinay.droid.salvatormundi.music.player
+
+import android.content.Context
+import com.google.android.exoplayer2.ExoPlayer
+
+interface MediaPlayer {
+
+    fun play(url: String)
+
+    fun getPlayerImpl(context: Context): ExoPlayer
+
+    fun releasePlayer()
+
+    fun setMediaSessionState(isActive: Boolean)
+}
---
+++ app/src/main/java/com/xinay/droid/salvatormundi/music/player/MediaPlayerImpl.kt
@@ -0,0 +1,24 @@
+package com.xinay.droid.salvatormundi.music.player
+
+import android.content.Context
+import com.google.android.exoplayer2.ExoPlayer
+
+class MediaPlayerImpl : MediaPlayer {
+
+
+    override fun play(url: String) {
+        TODO("not implemented")
+    }
+
+    override fun getPlayerImpl(context: Context): ExoPlayer {
+        TODO("not implemented")
+    }
+
+    override fun releasePlayer() {
+        TODO("not implemented")
+    }
+
+    override fun setMediaSessionState(isActive: Boolean) {
+        TODO("not implemented")
+    }
+}
